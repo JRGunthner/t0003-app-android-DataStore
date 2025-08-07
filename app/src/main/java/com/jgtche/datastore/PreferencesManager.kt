ff9c24b8b7093543cc4bed0cab3a3c8006392353
juliano rodrigues <dev@hdeletro.com.br>
2025-08-07 14:38:31 -0300

    Início

---
+++ app/src/main/java/com/jgtche/datastore/PreferencesManager.kt
@@ -0,0 +1,30 @@
+package com.jgtche.datastore
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.first
+
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "preferencesds")
+
+class PreferencesManager(private val context: Context) {
+    suspend fun save(key: String, value: String) {
+        val preferencesKey = stringPreferencesKey(key)
+        context.dataStore.edit { pref -> pref[preferencesKey] = value}
+    }
+
+    suspend fun read(key: String): String {
+        val preferencesKey = stringPreferencesKey(key)
+        val data = context.dataStore.data.first()
+
+        return data[preferencesKey] ?: ""
+    }
+
+    suspend fun remove(key: String, value: String) {
+        val preferencesKey = stringPreferencesKey(key)
+        context.dataStore.edit { pref -> pref.remove(preferencesKey)}
+    }
+}
